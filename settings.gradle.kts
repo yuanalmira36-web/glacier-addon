72782dc8c14ccf69a803ba96a29c6a028cda5aaa
yuanalmira36-web <yuanalmira36@gmail.com>
2026-05-22 13:29:41 +0000

    Patch

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,2 @@
-pluginManagement {
-    repositories {
-        maven("https://fabricmc.net")
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        maven("https://fabricmc.net")
-        maven("https://meteorclient.com")
-    }
-}
+pluginManagement { repositories { maven("https://fabricmc.net"); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositories { mavenCentral(); maven("https://fabricmc.net"); maven("https://meteorclient.com") } }
